8e59afb2ec38df45fd7913eb3d4bc8c8bda59a08
Seren <SerenTera@users.noreply.github.com>
2017-08-16 22:49:36 +0800

    Add JSON5 Implementation

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const Command = require('command'),
 		fs = require('fs'),
-		path= require('path')
+		path= require('path'),
+		JSON5 = require('./json5')
 
 //Defaults:
 let showinfo=true			//default show info of packet sent
 	version='*',			//default version
-	packet='S_CHAT' //default packet to dispatch
+	packet='S_ACTION_STAGE' //default packet to dispatch
 	
 module.exports = function dispatcher(dispatch) {
 	const command = Command(dispatch)
 	
 	let content
+	
 	
 	command.add('dispatchpkt', (pkt,ver) => {
 		packet=pkt
@@ -27,12 +29,24 @@ module.exports = function dispatcher(dispatch) {
 				command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
 				return
 			}
-			content=JSON.parse(data)
-			command.message('(Dispatcher) Copied packet data.')
+			content=JSON5.parse(data)
+			dispatch.toClient(packet,version,content)
+			command.message('(Dispatcher) Sent to Client. Packet:'+packet+' version:'+version)
+			if(showinfo) console.log('Packet info sent:'+JSON.stringify(content))
 		})
-		dispatch.toClient(packet,version,content)
-		command.message('(Dispatcher) Sent Packet:'+packet+' version:'+version)
-		if(showinfo) console.log('Packet info sent:'+JSON.stringify(content))
 	})
+
+	command.add('dispatchserver', () => {
+		fs.readFile(path.join(__dirname,'content.json'),function(err,data) {
+			if(err) {
+				command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
+				return
+			}
+			content=JSON5.parse(data)
+			dispatch.toServer(packet,version,content)
+			command.message('(Dispatcher) Sent to Server. Packet:'+packet+' version:'+version)
+			if(showinfo) console.log('Packet info sent:'+JSON.stringify(content))
+		})
+	})
+
 }
-	
