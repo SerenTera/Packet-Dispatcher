e53b3c81bfc2b1863e6a32686fda73a7cac1a9b6
Seren <cmingxuan94@gmail.com>
2018-08-22 17:25:13 +0800

    Refactorr

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Command = require('command'),
-		fs = require('fs'),
+const fs = require('fs'),
 		path= require('path'),
 		JSON5 = require('./json5')
 
@@ -8,43 +7,43 @@ let showinfo=true			//default show info of packet sent
 	version='*',			//default version
 	packet='S_ACTION_STAGE' //default packet to dispatch
 	
-module.exports = function dispatcher(dispatch) {
-	const command = Command(dispatch)
-	
+module.exports = function dispatcher(mod) {
+
 	let content
+		
 	
-	
-	command.add('dispatchpkt', (pkt,ver) => {
+/////Commands
+	mod.command.add('dispatchpkt', (pkt,ver) => {
 		packet=pkt
 		if(ver===undefined || isNaN(ver))
 			version='*'
 		else 
 			version=parseInt(ver)
-		command.message('(Dispatcher) Set Packet:'+packet+' version:'+version)
+		mod.command.message('(Dispatcher) Set Packet:'+packet+' version:'+version)
 	})
 	
-	command.add('dispatch', () => {
+	mod.command.add('dispatch', (name, ver) => {			
 		fs.readFile(path.join(__dirname,'content.json'),function(err,data) {
 			if(err) {
-				command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
+				mod.command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
 				return
 			}
 			content=JSON5.parse(data)
-			dispatch.toClient(packet,version,content)
-			command.message('(Dispatcher) Sent to Client. Packet:'+packet+' version:'+version)
+			mod.send(name === undefined ? packet : name , version === undefined ? version : ver,content)
+			mod.command.message('(Dispatcher) Sent to Client. Packet:'+name === undefined ? packet : name+' version:'+version === undefined ? version : ver)
 			if(showinfo) console.log('Packet info sent:'+JSON.stringify(content))
 		})
 	})
 
-	command.add('dispatchserver', () => {
+	mod.command.add('dispatchserver', () => {
 		fs.readFile(path.join(__dirname,'content.json'),function(err,data) {
 			if(err) {
-				command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
+				mod.command.message('(Dispatcher) Error reading file content.json. Check that it is in module folder')
 				return
 			}
 			content=JSON5.parse(data)
-			dispatch.toServer(packet,version,content)
-			command.message('(Dispatcher) Sent to Server. Packet:'+packet+' version:'+version)
+			mod.send(packet,version,content)
+			mod.command.message('(Dispatcher) Sent to Server. Packet:'+packet+' version:'+version)
 			if(showinfo) console.log('Packet info sent:'+JSON.stringify(content))
 		})
 	})
